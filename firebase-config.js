913bd792228c8709aecb8e8c38c78e919713df43
Harshil Patel <165708484+MastermindCore@users.noreply.github.com>
2025-07-18 19:34:56 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_cluzI8epSfItRlgbqUzRBl9mTUA1CIU",
+  authDomain: "robo-football.firebaseapp.com",
+  projectId: "robo-football",
+  storageBucket: "robo-football.firebasestorage.app",
+  messagingSenderId: "137309585195",
+  appId: "1:137309585195:web:be18ee9134d9c9605a4f6e",
+  measurementId: "G-95FB2R7HSW"
+};
