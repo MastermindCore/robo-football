52841b64165d074f3e828e97c55d794e121ec720
Harshil Patel <165708484+MastermindCore@users.noreply.github.com>
2025-07-18 19:36:23 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const fixtures = [
+  { match: 1, teamA: "Robo Kings", teamB: "Cyber Blasters", time: "10:00 AM", result: "2 - 1" },
+  { match: 2, teamA: "AI Strikers", teamB: "Byte Brawlers", time: "10:30 AM", result: "1 - 3" },
+  { match: 3, teamA: "Mech Ninjas", teamB: "Code Warriors", time: "11:00 AM", result: "0 - 0" },
+  { match: 4, teamA: "Team Phoenix", teamB: "Steel Storm", time: "11:30 AM" }
+];
