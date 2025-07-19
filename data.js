271d4e42f877200c93d7a6d610f1538ba405cfda
Harshil Patel <165708484+MastermindCore@users.noreply.github.com>
2025-07-19 11:28:07 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const fixtures = [
-  { match: 1, teamA: "Robo Kings", teamB: "Cyber Blasters", time: "10:00 AM", result: "2 - 1" },
+  { match: 1, teamA: "404 Not Found", teamB: "RobDynamo", time: "-", result: "-" },
   { match: 2, teamA: "AI Strikers", teamB: "Byte Brawlers", time: "10:30 AM", result: "1 - 3" },
   { match: 3, teamA: "Mech Ninjas", teamB: "Code Warriors", time: "11:00 AM", result: "0 - 0" },
   { match: 4, teamA: "Team Phoenix", teamB: "Steel Storm", time: "11:30 AM" }
